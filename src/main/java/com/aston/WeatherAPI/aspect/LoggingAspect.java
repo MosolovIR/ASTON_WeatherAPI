effab14101daeb450d323d278cb22a27d9f290cf
MosolovIR <mosolovolosom@gmail.com>
2024-12-09 20:39:53 +0300

    Logging added (Spring AOP)

---
+++ src/main/java/com/aston/WeatherAPI/aspect/LoggingAspect.java
@@ -0,0 +1,31 @@
+package com.aston.WeatherAPI.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    private static final Logger log = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Pointcut("execution(* com.aston.WeatherAPI.service.WeatherService.getWeather(..))")
+    public void getWeatherMethod() {}
+
+    @Before("getWeatherMethod() && args(cityName,..)")
+    public void logBefore(String cityName) {
+        log.info("Запрос погоды в городе " + cityName + ": метод getWeather() был вызван");
+    }
+
+    @After("getWeatherMethod()")
+    public void logAfter() {
+        log.info("Запрос погоды завершен: работа метода getWeather() завершена");
+    }
+
+    @AfterThrowing(value = "getWeatherMethod()", throwing = "exception")
+    public void logAfterThrowing(Exception exception) {
+        log.error("Ошибка при запросе погоды: " + exception.getMessage());
+    }
+}
