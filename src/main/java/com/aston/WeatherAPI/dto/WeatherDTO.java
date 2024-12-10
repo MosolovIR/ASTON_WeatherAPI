d7e09270fcbc99b2a20ca9d697e5c48bdd2f1e08
MosolovIR <mosolovolosom@gmail.com>
2024-12-10 20:56:13 +0300

    Lombok @Getter & @Setter fix

--- src/main/java/com/aston/WeatherAPI/dto/WeatherDTO.java
+++ src/main/java/com/aston/WeatherAPI/dto/WeatherDTO.java
@@ -1,64 +1,27 @@
 package com.aston.WeatherAPI.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class WeatherDTO {
     private Location location;
     private Current current;
 
-    public Location getLocation() {
-        return location;
-    }
-
-    public void setLocation(Location location) {
-        this.location = location;
-    }
-
-    public Current getCurrent() {
-        return current;
-    }
-
-    public void setCurrent(Current current) {
-        this.current = current;
-    }
-
+    @Setter
+    @Getter
     public static class Location {
         private String name;
 
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
     }
 
+    @Setter
+    @Getter
     public static class Current {
         private int temperature;
         private int pressure;
         private int humidity;
 
-        public int getTemperature() {
-            return temperature;
-        }
-
-        public void setTemperature(int temperature) {
-            this.temperature = temperature;
-        }
-
-        public int getPressure() {
-            return pressure;
-        }
-
-        public void setPressure(int pressure) {
-            this.pressure = pressure;
-        }
-
-        public int getHumidity() {
-            return humidity;
-        }
-
-        public void setHumidity(int humidity) {
-            this.humidity = humidity;
-        }
     }
 }
